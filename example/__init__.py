7cfe536a501e03192dd57a793c1967b1fdb4b06f
Ronald Miller <ronald.miller@me.com>
2023-09-21 23:52:24 -0400

    add asset deps, plots

--- example/__init__.py
+++ example/__init__.py
@@ -1,14 +1,10 @@
-from dagster import Definitions, load_assets_from_package_module
+from dagster import Definitions, load_assets_from_modules
 
-# from .assets import metrics, trips
+from example import assets
 
-from example.assets import topstory_ids
-
-
-# all_assets = load_assets_from_package_module(package_module=example, group_name="core")
-# all_assets = load_assets_from_modules([assets])
+all_assets = load_assets_from_modules([assets])
 
 
 defs = Definitions(
-    assets=[topstory_ids]
+    assets=all_assets
 )
--- example/assets.py
+++ example/assets.py
@@ -1,14 +1,41 @@
-import json
-import os
 
-import requests
-from dagster import asset
+import pandas as pd
+
+import matplotlib.pyplot as plt
+from pandas import DataFrame
+from io import BytesIO
+import base64
+
+
+from dagster import asset, Output, AssetExecutionContext, MetadataValue
+
+def countries_bar_graph(df: DataFrame) -> str:
+    plt.figure(figsize=(10, 6))
+    plt.bar(df.index, df["Population"])
+    plt.xlabel('Country')
+    plt.ylabel('Population')
+    plt.title("Top 10 Most Populated Countries")
+    plt.tight_layout()
+
+    with BytesIO() as buffer:
+        plt.savefig(buffer, format="png")
+        image_data = base64.b64encode(buffer.getvalue())
+
+        # Convert the image to Markdown
+        md_content = f"![img](data:image/png;base64,{image_data.decode()})"
+
+    return md_content
 
 @asset
-def topstory_ids() -> None:
-    newstories_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
-    top_new_story_ids = requests.get(newstories_url).json()[:100]
+def country_population() -> pd.DataFrame:
+    return pd.read_html(
+        "https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population"
+    )[0]
 
-    os.makedirs("data", exist_ok=True)
-    with open("data/topstory_ids.json", "w") as f:
-        json.dump(top_new_story_ids, f)
+
+@asset
+def country_population_meta(context: AssetExecutionContext, country_population: pd.DataFrame) -> Output[pd.DataFrame]:
+    top_10 = country_population[["Country / Dependency","Population"]].sort_values("Population", ascending=False).iloc[1:10].set_index("Country / Dependency")
+    md_content = countries_bar_graph(top_10)
+    context.add_output_metadata(metadata={"plot": MetadataValue.md(md_content)})
+    return Output(country_population, metadata={"num_rows": len(country_population)})
