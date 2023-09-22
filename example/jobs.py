7de7fa8d83d3004c16a4e8d6f0559017bf617222
Ronald Miller <ronald.miller@me.com>
2023-09-21 22:46:08 -0400

    deps and examples setup

---
+++ example/jobs.py
@@ -0,0 +1,6 @@
+from dagster import job
+
+
+@job
+def test():
+    pass
---
+++ example/schedules.py
@@ -0,0 +1,14 @@
+"""Collection of Cereal schedules"""
+
+from dagster import schedule
+
+from example.jobs import test
+
+@schedule(
+    cron_schedule="0 9 * * 1-5",
+    job=test,
+)
+def every_weekday_9am(context):
+    """Example of how to setup a weekday schedule for a job."""
+    date = context.scheduled_execution_time.strftime("%Y-%m-%d")
+    return {"ops": {"download_cereals": {"config": {"date": date}}}}
